31760790a925c0a41d79a46cb2fcac8e503ecfab
Nomanpervaiz <nomanpervaiz035@gmail.com>
2025-01-25 23:27:26 +0500

    Revert "added second comit"
    This reverts commit 0bce0bd5aaa2fabe6f64a705ee5a7faf450c3ea5.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,16 @@
-import express from "express";
-import authRouter from "../routes/auth.js";
-import beneficiaryRouter from "../routes/beneficiary.js";
-import tokenRouter from "../routes/token.js";
+import express from 'express'
+const router = express.Router()
+import authRouter from "../routes/auth.js"
+import beneficiaryRouter from "../routes/beneficiary.js"
+import tokenRouter from "../routes/token.js"
 
-const router = express.Router();
+router.get("/" , (req,res)=> res.status(200).json({
+    error : false , 
+    message : "Welcome to the API"
+}))
+router.use("/auth" , authRouter )
+router.use("/beneficiary" , beneficiaryRouter )
+router.use("/token" , tokenRouter )
 
-router.get("/", (req, res) =>
-  res.status(200).json({
-    error: false,
-    message: "Welcome to the API",
-  })
-);
-router.use("/auth", authRouter);
-router.use("/beneficiary", beneficiaryRouter);
-router.use("/token", tokenRouter);
+export default router
 
-export default router;
